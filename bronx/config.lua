5c2253ef7b76d345cbbea422cc4b9497ac797bdd
m <mjkzyalt@gmail.com>
2022-04-09 13:45:42 -0500

    move bronx 1.0

---
+++ bronx/config.lua
@@ -0,0 +1,33 @@
+include("__init__")
+
+gamehost = "name"
+hostprestige = 30
+
+guest1 = "name"
+g1prestige = 30
+
+guest2 = "name"
+g2prestige = 30
+
+guest2 = "name"
+g3prestige = 30
+
+guest4 = "name"
+g4prestige = 30
+
+guest5 = "name"
+g5prestige = 30
+
+--[[
+=======YOU MUST AUTH EACH PLAYER BY NAME; CUSTOM PRESTIGE IS OPTIONAL=======
+
+    +++IN GAME BINDS+++
+
+    ALL PLAYERS:
+    *Crouch + Knife = Refill Ammo and Grenades
+
+    HOST ONLY:
+    *Crouch + Dpad Left(+actionslot 3) = Save Bot Spawn/Activate Setup Mode
+    *Prone + Knife = Clear Bot Spawn/Activate Unsetup Mode
+
+]] -- 
